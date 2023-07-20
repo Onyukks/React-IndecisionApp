85e066306f7a80c7f307b3eb550c0265e399157b
Onyeukwu <onyeukwuagbafo@gmail.com>
2023-07-20 09:32:30 +0100

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Action from "./components/Action";
+import Options from "./components/Options";
+import AddOption from "./components/AddOption"
+import OptionModal from "./components/OptionModal";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class IndecisionApp extends React.Component {
+    state={
+        options:[],
+        selectedOption:undefined
+    }
+   handleSelectedOption = ()=>{
+     this.setState(()=>({selectedOption:undefined}))
+   }
+    handleDeleteOptions =()=> {
+        this.setState(() => ({ options: [] }));
+      }
+      handleDeleteOption = optionToRemove => {
+        this.setState((prevState) => ({
+          options: prevState.options.filter((option) => optionToRemove !== option)
+        }));
+      }
+      handlePick =()=> {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[randomNum];
+        this.setState(()=>({selectedOption:option}))
+      }
+      handleAddOption = option => {
+        if (!option) {
+          return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+          return 'This option already exists';
+        }
+    
+        this.setState((prevState) => ({
+          options: prevState.options.concat(option)
+        }));
+      }
+
+    componentDidMount() {
+      try {
+        const json = localStorage.getItem('options');
+        const options = JSON.parse(json);
+  
+        if (options) {
+          this.setState(() => ({ options }));
+        }
+      } catch (e) {
+        // Do nothing at all
+      }
+    }
+    componentDidUpdate(prevProps, prevState) {
+      if (prevState.options.length !== this.state.options.length) {
+        const json = JSON.stringify(this.state.options);
+        localStorage.setItem('options', json);
+      }
+    }
+    componentWillUnmount() {
+      console.log('componentWillUnmount');
+    }
+   
+    render() {
+        const title = 'Indecision'
+      const subtitle = 'Put your life in the hands of a computer';
+  
+      return (
+        <div>
+          <Header title={title} subtitle={subtitle} />
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+         <OptionModal selectedOption={this.state.selectedOption} handleSelectedOption={this.handleSelectedOption}/>
+        </div>
+      );
+    }
+  }
